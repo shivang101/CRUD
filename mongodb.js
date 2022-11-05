e5f158fbde9211f80352012c454ac59a625f33c3
shivang <shivangmathur101@gmail.com>
2022-11-05 21:35:38 +0530

    added Create method

--- mongodb.js
+++ mongodb.js
@@ -15,5 +15,61 @@ MongoClient.connect(
       return console.log(error);
     }
     console.log(`Connected Correctly`);
+    const db = client.db(databaseName);
+
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Shivang",
+    //     age: 21,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert error");
+    //     }
+    //     console.log(result.insertedId);
+    //   }
+    // );
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Shivang",
+    //       age: 21,
+    //     },
+    //     {
+    //       name: "Arnav",
+    //       age: 15,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     {
+    //       if (error) {
+    //         return console.log("Unable to insert error");
+    //       }
+    //       console.log(result.insertedIds, result.insertedCount);
+    //     }
+    //   }
+    // );
+    db.collection("tasks").insertMany(
+      [
+        {
+          description: "Task 1",
+          completed: true,
+        },
+        {
+          description: "Task 2",
+          completed: false,
+        },
+        {
+          description: "Task 3",
+          completed: true,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to connect");
+        }
+        console.log(result.insertedCount, result.insertedIds);
+      }
+    );
   }
 );
