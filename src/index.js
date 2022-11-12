51abc3a1a655ee1270f09777e979af73ddfc7040
shivang <shivangmathur101@gmail.com>
2022-11-12 16:11:40 +0530

    Completed CRUD Operations

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
 const Task = require("./models/task");
+const { findByIdAndDelete } = require("./models/task");
 
 const app = express();
 
@@ -9,105 +10,181 @@ const port = process.env.port || 3000;
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  User.find({})
-    .then((result) => {
-      res.send(result);
-    })
-    .then((error) => {
-      res.status(500).send(error.message);
-    });
-});
-
-app.get("/users/:id", (req, res) => {
-  const _id = req.params.id;
-  User.findById(_id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).send();
-      }
-      res.send(user);
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
-});
-
-app.get("/tasks", (req, res) => {
-  Task.find({})
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((error) => {
-      res.status(500).send(error.message);
-    });
-});
-
-app.get("/tasks/:id", (req, res) => {
-  const _id = req.params.id;
-  Task.findById(_id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.send(task);
-    })
-    .catch((error) => {
-      res.status(500).send();
-    });
-});
-
-app.get("/tasks/:status", (req, res) => {
-  const completed = req.params;
-  let flag;
-  if (completed === true) {
-    flag = true;
-  } else {
-    flag = false;
-  }
-  console.log(req.params.status);
-
-  Task.find({ completed: flag })
-    .then((result) => {
-      // res.send("here");
-      res.status(200).send(result);
-    })
-    .catch((error) => {
-      res.status(500).send(error.message);
-    });
-});
-
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body);
 
-  user
-    .save()
-    .then((result) => {
-      //console.log(result);
-      //same thing
-      console.log(user);
+  try {
+    await user.save();
+    res.status(201).send(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
 
-      res.status(201).send(result);
-    })
-    .catch((error) => {
-      res.status(400).send(error.message);
-      res.send(error);
-    });
+  // user
+  //   .save()
+  //   .then((result) => {
+  //     //console.log(result);
+  //     //same thing
+  //     console.log(user);
+
+  //     res.status(201).send(result);
+  //   })
+  //   .catch((error) => {
+  //     res.status(400).send(error.message);
+  //     res.send(error);
+  //   });
 });
 
-app.post("/tasks", (req, res) => {
+app.patch("/users/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password", "age"];
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates" });
+  }
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+app.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.send(404).send();
+    }
+    res.status(200).send(user);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
-
-  task
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).send(result);
-    })
-    .catch((error) => {
-      res.status(400).send(error.message);
-    });
+  try {
+    task.save();
+    res.status(201).send(task);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
+
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdates.includes(update);
+  });
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: "Invalid updates" });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).send(tasks);
+  } catch (e) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.get("/tasks/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const task = await Task.findById(_id);
+    if (!task) {
+      return res.status(400).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.send(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.send(e);
+  }
+});
+
+// app.get("/tasks/:status", (req, res) => {
+//   const completed = req.params;
+//   console.log("here");
+
+//   let flag;
+//   if (completed === true) {
+//     flag = true;
+//   } else {
+//     flag = false;
+//   }
+//   console.log(req.params.status);
+
+//   Task.find({ completed: flag })
+//     .then((result) => {
+//       // res.send("here");
+//       res.status(200).send(result);
+//     })
+//     .catch((error) => {
+//       res.status(500).send(error.message);
+//     });
+// });
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
